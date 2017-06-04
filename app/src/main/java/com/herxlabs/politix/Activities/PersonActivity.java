7d616b51e19076ccba3c55b0bbc4aa6aab5b7a5e
Heriberto Arias Torres <er.mx123@gmail.com>
2017-06-03 22:02:31 -0500

    Cambios en colores de la app Se mejora el sistema de filtrado de funcionarios

--- app/src/main/java/com/herxlabs/politix/Activities/PersonActivity.java
+++ app/src/main/java/com/herxlabs/politix/Activities/PersonActivity.java
@@ -3,14 +3,11 @@ package com.herxlabs.politix.Activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -60,14 +57,13 @@ public class PersonActivity extends AppCompatActivity {
         if(politico.getYoutube().isEmpty()) lSocial.removeView(cardYoutube);
         if(politico.getEmail().isEmpty()) lSocial.removeView(cardCorreo);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Marcado: " + politico.getEmail(), Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
 
     }
     @OnClick(R.id.facebook)
--- app/src/main/java/com/herxlabs/politix/Activities/SenadoresActivity.java
+++ app/src/main/java/com/herxlabs/politix/Activities/SenadoresActivity.java
@@ -2,24 +2,21 @@ package com.herxlabs.politix.Activities;
 
 
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.view.inputmethod.EditorInfo;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.herxlabs.politix.Adapters.SenadoresAdapter;
@@ -36,15 +33,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SenadoresActivity extends AppCompatActivity  {
+public class SenadoresActivity extends AppCompatActivity  implements SearchView.OnQueryTextListener{
+    private SearchView searchView;
+    private MenuItem searchMenuItem;
     private List<Politico> senadoresList = new ArrayList<>();
     private RecyclerView recyclerView;
     private SenadoresAdapter pAdapter;
     private ProgressDialog pDialog;
-    @BindView(R.id.txtSearch) TextView txtSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,17 +71,8 @@ public class SenadoresActivity extends AppCompatActivity  {
                     }
                 })
         );
-        txtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    filter();
-                    return true;
-                }
-                return false;
-            }
-        });
         new GetSenadores().execute();
+        handleIntent(getIntent());
 
     }
 
@@ -92,9 +80,36 @@ public class SenadoresActivity extends AppCompatActivity  {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_senadores, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchMenuItem = menu.findItem(R.id.searchBar);
+        searchView = (SearchView) searchMenuItem.getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
         return true;
     }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+    private void handleIntent(Intent intent) {
 
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            filter(query);
+        }
+    }
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        filter(newText);
+        return true;
+    }
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -114,8 +129,7 @@ public class SenadoresActivity extends AppCompatActivity  {
         return super.onOptionsItemSelected(item);
     }
 
-    void filter(){
-        String text = txtSearch.getText().toString();
+    void filter(String text){
         pAdapter.getFilter().filter(text);
         recyclerView.scrollToPosition(0);
         recyclerView.setAlpha(0.0f);
@@ -197,28 +211,6 @@ public class SenadoresActivity extends AppCompatActivity  {
             recyclerView.setAdapter(pAdapter);
             pAdapter.sortByNombre();
             pAdapter.notifyDataSetChanged();
-            txtSearch.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-                    // TODO Auto-generated method stub
-                }
-
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                    // TODO Auto-generated method stub
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                new Handler().postDelayed(new Runnable() {
-                    public void run() {
-                        filter();
-                    }
-                }, 450);
-                }
-            });
         }
 
     }
